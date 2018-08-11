e27914c14afd418b6b91767b94b3891913b3decb
Oliver De La Via <odelavia@gmail.com>
2018-08-11 02:39:11 -0400

    changed the db env to elephantsql for easier use

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     SERVER_NAME: 'https://face-tagger.herokuapp.com/',
     ClarifaiApiKey: process.env.ClarifaiApiKey,
-    DB: process.env.DATABASE_URL,
+    DB: process.env.DB,
     JWT: process.env.JWT,
     REDIS_URL: process.env.REDIS_URL
 }
