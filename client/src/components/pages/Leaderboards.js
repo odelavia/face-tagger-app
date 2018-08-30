13e0f38ac546bcdfc0ba71e9d1e5b9f6885e5180
Oliver De La Via <odelavia@gmail.com>
2018-08-30 03:15:19 -0400

    added basic styling to leaderboards table

--- client/src/components/pages/Leaderboards.js
+++ client/src/components/pages/Leaderboards.js
@@ -24,23 +24,25 @@ class Leaderboards extends Component {
   render() {
     let loadUsers = this.state.users.map( (user, index) => {
       return (
-        <div key={index}>
-          <div>{user.name}</div>
-          <div>{user.entries}</div>
-          <div>{user.joined}</div>
-          <div>{user.age}</div>
+        <div key={index} className="table-rows">
+          <div className="table-row">
+            <div>{user.name}</div>
+            <div>{user.entries}</div>
+            <div>{user.joined}</div>
+            <div>{user.age}</div>
+          </div>
         </div>
       );
     })
     return (
       <Fragment>
       <h1>Leaderboards</h1>
-      <div>
+      <div className="leaderboard-table">
         <div className="col-names">
-          <div>name</div>
-          <div>rank</div>
-          <div>joined</div>
-          <div>age</div>
+          <div className="col-name first">name</div>
+          <div className="col-name">rank</div>
+          <div className="col-name">joined</div>
+          <div className="col-name last">age</div>
         </div>
         {loadUsers}
       </div>
